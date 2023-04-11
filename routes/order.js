c4309d7db03090cd1d020323cace085dfab70714
Hakan <hakantekir2002@outlook.com>
2023-04-11 15:48:20 +0300

    feat: Simplify order function

--- routes/order.js
+++ routes/order.js
@@ -6,59 +6,92 @@ const con = require('../utils/sql');
 router.post('/', function (req, res){
     let userId = req.query.userId, addressId = req.query.addressId
     let response = {}
+    getCartItems(userId, function(err, coffees) {
+        if (err) {
+            response.status = 1
+            console.log(response)
+            return res.send(response)
+        }
+        console.log(coffees)
+        createOrder(userId, addressId, function(err, orderId) {
+            if (err) {
+                response.status = 1
+                console.log(response)
+                return res.send(response)
+            }
+            insertOrderDetails(orderId, coffees, function(err) {
+                if (err) {
+                    response.status = 1
+                    console.log(response)
+                    return res.send(response)
+                }
+                deleteCart(userId, function(err) {
+                    if (err) {
+                        response.status = 1
+                        console.log(response)
+                        return res.send(response)
+                    }
+                    response.status = 0
+                    console.log(response)
+                    res.send(response)
+                })
+            })
+        })
+    })
+})
 
+function getCartItems(userId, callback) {
     let sql = "SELECT userId, coffeeId, quantity, price FROM Cart INNER JOIN Coffee ON Cart.coffeeId = Coffee.id WHERE userId = ?"
     let filter = [userId]
     con.query(sql, filter, function (err, result) {
         if (err) {
-            response.status = 1
-            res.send(response)
+            callback(err)
         } else {
-            if (result.length > 0) {
-                let coffees = result
-                let sql = "INSERT INTO iCoffee.Order (userid, addressid, orderdate) VALUES (?, ? ,curdate())"
-                let dateString = date.toString()
-                let filter = [userId, addressId, dateString]
-                con.query(sql, filter, function (err, result) {
-                    if (err) {
-                        response.status = 1
-                        res.send(response)
-                        console.log(response)
-                    } else {
-                        let orderId = result.insertId
-                        let filter = []
-                        let sql = "INSERT INTO iCoffee.OrderDetails (orderId, coffeeId, quantity, price) VALUES ?"
-                        for (let i = 0; i < coffees.length; i++) {
-                            filter[i] = [orderId, coffees[i].coffeeId, coffees[i].quantity, coffees[i].price]
-                        }
-                        console.log(filter)
-                        con.query(sql, [filter], function (err, result) {
-
-                            if (err) {
-                                response.status = 1
-                                res.send(response)
-                                console.log(response)
-                            } else {
-                                let sql = "DELETE FROM Cart WHERE userId = ?"
-                                let filter = [userId]
-                                con.query(sql, filter, function (err, result) {
-                                    if (err) {
-                                        response.status = 1
-                                        res.send(response)
-                                        console.log(response)
-                                    } else {
-                                        response.status = 0
-                                        res.send(response)
-                                        console.log(response)
-                                    }
-                                })
-                            }
-                        })
-                    }
-                })
-            }
+            callback(null, result)
         }
     })
-})
+}
+
+function createOrder(userId, addressId, callback) {
+    let sql = "INSERT INTO iCoffee.Order (userid, addressid, orderdate) VALUES (?, ? ,curdate())"
+    let dateString = date.toString()
+    let filter = [userId, addressId, dateString]
+    con.query(sql, filter, function (err, result) {
+        if (err) {
+            callback(err)
+        } else {
+            let orderId = result.insertId
+            callback(null, orderId)
+        }
+    })
+}
+
+function insertOrderDetails(orderId, coffees, callback) {
+    let filter = []
+    let sql = "INSERT INTO iCoffee.OrderDetails (orderId, coffeeId, quantity, price) VALUES ?"
+    for (let i = 0; i < coffees.length; i++) {
+        filter[i] = [orderId, coffees[i].coffeeId, coffees[i].quantity, coffees[i].price]
+    }
+    con.query(sql, [filter], function (err, result) {
+        if (err) {
+            callback(err)
+        } else {
+            callback(null)
+        }
+    })
+}
+
+function deleteCart(userId, callback) {
+    let sql = "DELETE FROM Cart WHERE userId = ?"
+    let filter = [userId]
+    con.query(sql, filter, function (err, result) {
+        if (err) {
+            callback(err)
+        } else {
+            callback(null)
+        }
+    })
+}
+
 
 module.exports = router;
